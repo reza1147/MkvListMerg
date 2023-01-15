e7fd66c7c26d4fa2634dd32010db744223cd67e3
reza147 <reza.mohammadzadeh.1997@gmail.com>
2023-01-15 10:15:51 +0330

    redesign Directory model and fix concurrency

---
+++ src/main/java/Service/MakeDirectoryViewTask.java
@@ -0,0 +1,25 @@
+package Service;
+
+import Model.Directory;
+import View.DirectoryView;
+import ViewModel.DirectoryViewModel;
+import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.ViewTuple;
+import javafx.concurrent.Task;
+
+import java.util.List;
+
+public class MakeDirectoryViewTask extends Task<ViewTuple<DirectoryView, DirectoryViewModel>> {
+    Directory model;
+
+    public MakeDirectoryViewTask(Directory model) {
+        this.model = model;
+    }
+
+    @Override
+    protected ViewTuple<DirectoryView, DirectoryViewModel> call() throws Exception {
+        ViewTuple<DirectoryView, DirectoryViewModel> viewTuple = FluentViewLoader.fxmlView(DirectoryView.class).load();
+        viewTuple.getViewModel().initWithModel(model);
+        return viewTuple;
+    }
+}
